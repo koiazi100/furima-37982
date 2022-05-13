94709e2b7f6efac73abbea4b990893ee20d7ff50
koiazi100 <wbc_iwamura@yahoo.co.jp>
2022-05-13 17:50:31 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :title, presence: true
+         validates :explanation, presence: true
+         validates :price, presence: true
+         validates :category_id, presence: true
+         validates :status_id, presence: true
+         validates :fee_burden_id, presence: true
+         validates :ship_day_id, presence: true
+         validates :ship_area_id, presence: true
+         validates :user_id, presence: true
+
 end
