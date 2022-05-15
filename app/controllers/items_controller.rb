d5f222ddec9502d37f8623ad4197f4e5ad55cef8
koiazi100 <wbc_iwamura@yahoo.co.jp>
2022-05-15 17:17:41 +0900

    アクティブストレージ導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit([:image, :title, :explanation, :category_id, :status_id, :fee_burden_id, :ship_area_id, :ship_day_id]).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,8 @@ class Item < ApplicationRecord
   validates :ship_day_id, presence: true
   validates :ship_area_id, presence: true
   validates :user_id, presence: true
+
+  has_one_attached :image
+  belong_to :user
+  has_one  :purchase_record
 end
