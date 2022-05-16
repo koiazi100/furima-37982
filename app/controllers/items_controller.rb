837b915e621d092e6fd46b738724334e850f96bc
koiazi100 <wbc_iwamura@yahoo.co.jp>
2022-05-16 16:05:41 +0900

    出品商品保存・手数料計算実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,29 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
+  end
+  
+  def new
+    @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end 
+
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  
   private
 
   def item_params
-    params.require(:item).permit([:image, :title, :explanation, :category_id, :status_id, :fee_burden_id, :ship_area_id, :ship_day_id]).merge(user_id: current_user.id)
+    params.require(:item).permit([:image, :title,:price, :explanation, :category_id, :status_id, :fee_burden_id, :ship_area_id, :ship_day_id]).merge(user_id: current_user.id)
   end
 end
