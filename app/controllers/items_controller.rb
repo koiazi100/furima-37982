bfd446648edfd63c944e14b70c875267f2e30a05
koiazi100 <wbc_iwamura@yahoo.co.jp>
2022-05-17 20:29:41 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_sign_up, only: [:new, :edit, :destroy]
+  before_action :move_to_sign_up, only: [:new, :edit]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.order('created_at DESC')
@@ -34,8 +34,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
