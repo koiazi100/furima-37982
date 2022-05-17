9108240b8b9d61536e7ccfbf3dccac53c9bad2ec
koiazi100 <wbc_iwamura@yahoo.co.jp>
2022-05-17 18:52:53 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_sign_up, only: [:new, :edit]
-
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,18 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      render :show
+      redirect_to item_path
     else
       render :edit
     end
@@ -45,5 +42,9 @@ class ItemsController < ApplicationController
 
   def move_to_sign_up
     redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
