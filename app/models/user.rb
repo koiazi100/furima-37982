ba8c16cda91527afc2d7afb1d1de1d2386b29122
koiazi100 <wbc_iwamura@yahoo.co.jp>
2022-05-22 20:27:31 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+         
+with_options presence: true do
+  validates :nickname
+  validates :last_name
+  validates :first_name
+  validates :last_name_kana
+  validates :first_name_kana
+  validates :birthday
+end
+validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, allow_blank: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, allow_blank: true
+  validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
+  validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, confirmation: true
+  validates_format_of :password, with: PASSWORD_REGEX, confirmation: true, allow_blank: true
 end
